950bf4b2437fd927f04d60193c0d8d380f17c068
Fredrik Svensson <svensson.jf@gmail.com>
2026-02-11 14:27:51 +0100

    Add initial database package exports for Prisma client, mappers, and repositories

---
+++ packages/db/src/index.ts
@@ -0,0 +1,10 @@
+// Database package exports
+
+// Prisma client
+export { prisma, PrismaClient } from "./client.js";
+
+// Mappers
+export * from "./mappers.js";
+
+// Repositories
+export * from "./repositories/index.js";
