e6fae889b35e66a6d96efca22e7599dee1a49f13
Fredrik Svensson <svensson.jf@gmail.com>
2026-02-14 13:06:14 +0100

    Add export for DocumentRepository in index.ts

--- packages/db/src/repositories/index.ts
+++ packages/db/src/repositories/index.ts
@@ -2,3 +2,4 @@ export { OrganizationRepository } from "./organization.repository.js";
 export { VoucherRepository } from "./voucher.repository.js";
 export { AccountRepository } from "./account.repository.js";
 export { FiscalYearRepository } from "./fiscal-year.repository.js";
+export { DocumentRepository } from "./document.repository.js";
