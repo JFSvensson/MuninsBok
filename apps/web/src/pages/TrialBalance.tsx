b670986057762ae6c7680d5e9cb6a712f68d6e56
Fredrik Svensson <svensson.jf@gmail.com>
2026-02-17 15:40:17 +0100

    Add date range filtering to TrialBalance component

--- apps/web/src/pages/TrialBalance.tsx
+++ apps/web/src/pages/TrialBalance.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useOrganization } from "../context/OrganizationContext";
 import { api } from "../api";
 import { formatAmount, amountClassName } from "../utils/formatting";
 import { toCsv, downloadCsv, csvAmount } from "../utils/csv";
+import { DateFilter, type DateRange } from "../components/DateFilter";
 
 export function TrialBalance() {
   const { organization, fiscalYear } = useOrganization();
+  const [dateRange, setDateRange] = useState<DateRange | undefined>();
 
   const { data, isLoading, error } = useQuery({
-    queryKey: ["trial-balance", organization?.id, fiscalYear?.id],
-    queryFn: () => api.getTrialBalance(organization!.id, fiscalYear!.id),
+    queryKey: ["trial-balance", organization?.id, fiscalYear?.id, dateRange],
+    queryFn: () => api.getTrialBalance(organization!.id, fiscalYear!.id, dateRange),
     enabled: !!organization && !!fiscalYear,
   });
 
@@ -54,6 +57,9 @@ export function TrialBalance() {
         >
           Exportera CSV
         </button>
+      </div>
+      <div className="mb-2">
+        <DateFilter onFilter={setDateRange} />
       </div>
       <table>
         <thead>
