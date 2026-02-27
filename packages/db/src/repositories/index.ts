e59e6b5dd22a6b88b656de20737d8c83a6dead2e
Fredrik Svensson <svensson.jf@gmail.com>
2026-02-27 14:41:39 +0100

    Add RefreshTokenRepository export to index file

--- packages/db/src/repositories/index.ts
+++ packages/db/src/repositories/index.ts
@@ -4,3 +4,4 @@ export { AccountRepository } from "./account.repository.js";
 export { FiscalYearRepository } from "./fiscal-year.repository.js";
 export { DocumentRepository } from "./document.repository.js";
 export { UserRepository } from "./user.repository.js";
+export { RefreshTokenRepository } from "./refresh-token.repository.js";
