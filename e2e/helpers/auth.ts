14d15e90f03eee33464987249f21cbfb358c7efe
Fredrik Svensson <svensson.jf@gmail.com>
2026-03-26 09:30:37 +0100

    Fix API base URL in auth helper to use 127.0.0.1

--- e2e/helpers/auth.ts
+++ e2e/helpers/auth.ts
@@ -6,7 +6,7 @@
  */
 import type { Page, APIRequestContext } from "@playwright/test";
 
-const API_BASE = "http://localhost:3000";
+const API_BASE = "http://127.0.0.1:3000";
 const TEST_PASSWORD = "TestPass123!";
 
 let counter = 0;
